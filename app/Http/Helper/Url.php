ae31d3372832fad4c2b6d269db15b68ccde167bb
thanglk90 <lykimthang22@gmail.com>
2022-03-07 00:24:58 +0700

    sua lai duong link o file URL va filesystem, ly do vi link da point thang vao folder public, nen disk public.url ko can phai vao public nua

--- app/Http/Helper/Url.php
+++ app/Http/Helper/Url.php
@@ -6,7 +6,9 @@ class Url {
 
     public static function linkThumb($thumb){
 
-        $thumb = route('client-home') . '/storage/public/photos/shares/products/' . $thumb;
+        // $thumb = route('client-home') . '/storage/public/photos/shares/products/' . $thumb;
+        $thumb = route('client-home') . '/storage/photos/shares/products/' . $thumb; // chạy trên local do đã trỏ sẵn vào folder public nên ko cần /storage/public/photos
+        
         // $thumb = route('client-home') . '/storage/photos/shares/products/' . $thumb; -> if public to host
         return $thumb;
     }
